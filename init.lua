6925053debececb7e5a15ebbf6c239ee381af469
HartmutFritz <hartmut.fritz@proton.me>
2026-08-20 15:37:43 +0200

    _

--- init.lua
+++ init.lua
@@ -626,8 +626,12 @@ require('lazy').setup({
           settings = {
             pylsp = {
               plugins = {
-                pycodestyle = {
-                  enabled = false,
+                pycodestyle = { enabled = false },
+                ruff = {
+                  enabled = true,
+                  -- Linting only, no formatting:
+                  format = { 'I' }, -- only import sorting, or set to {} for no formatting
+                  lineLength = 88,
                 },
               },
             },
@@ -678,30 +682,14 @@ require('lazy').setup({
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-      -- require('mason-lspconfig').setup {
-      --   handlers = {
-      --     function(server_name)
-      --       local server = servers[server_name] or {}
-      --       -- This handles overriding only values explicitly passed
-      --       -- by the server configuration above. Useful when disabling
-      --       -- certain features of an LSP (for example, turning off formatting for ts_ls)
-      --       server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-      --       require('lspconfig')[server_name].setup(server)
-      --     end,
-      --   },
-      -- }
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
-            -- block Mason-managed servers you don't want
-            if server_name == 'pylsp' or server_name == 'clangd' then
-              return
-            end
-
             local server = servers[server_name] or {}
-
+            -- This handles overriding only values explicitly passed
+            -- by the server configuration above. Useful when disabling
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-
             require('lspconfig')[server_name].setup(server)
           end,
         },
